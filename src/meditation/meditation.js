44e3801a40f464d682c684df1f26e549df482d5f
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2024-06-06 16:28:34 +0200

    Correzione
    Correzione progetto

---
+++ src/meditation/meditation.js
@@ -0,0 +1,58 @@
+import React from "react"
+import Header from "./Header"
+import FormWatch from "./Timer"
+import Footer from "./Footer"
+import "./assets/css/style.css"
+
+class Meditation extends React.Component {
+
+  // Oggetto orologio
+  constructor(props){
+    super(props);
+    //State
+    this.state = {
+      light: true
+    }
+    // Metodi
+    this.handleMode =  this.handleMode.bind(this);
+  }
+
+  // Dichiarazione dei metodi (Cambio mode)
+  handleMode(event) {
+    this.setState({
+      light: !this.state.light,
+    })
+  }
+
+  // Visualizzazione
+  render() {
+    return(
+      <>
+      <header className={this.state.light ? "light-header" : "dark-header"}>
+      <Header />
+      </header>
+
+      <nav className={this.state.light ? "light-nav" : "dark-nav"}>
+        <button onClick={this.handleMode} className="mode-button">
+        {this.state.light ? "light" : "dark"}
+        </button>
+      </nav>
+
+      <main className={this.state.light ? "light-main" : "dark-main"}>
+        <div className="container">
+          <p clasName="description">Click on <b>Start</b> button to start your meditation</p>
+          <FormWatch />
+        </div>
+      </main>
+
+      <footer className={this.state.light ? "light-footer" : "dark-footer"}>
+        <Footer />
+      </footer>
+
+      </>
+    )
+  }
+}
+
+
+export default Meditation;
